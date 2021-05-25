57f1c35b1df8206c295eddb282f8bc2eac55a4a5
Phil McCarthy <philmcc2631@gmail.com>
2021-05-25 11:00:08 -0600

    Add dragon solutions

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,21 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.eat_count = 0;
+    this.hungry = true;
+  }
 
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+
+  eat() {
+    this.eat_count++;
+    if (this.eat_count >= 3) {
+      this.hungry = false;
+    }
+  }
+}
+
+module.exports = Dragon;
