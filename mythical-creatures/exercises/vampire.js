a99403a8222ffecf7a16fc8e804380ddd359e946
Phil McCarthy <philmcc2631@gmail.com>
2021-05-25 11:12:42 -0600

    Add vampire solutions

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -1,6 +1,18 @@
 class Vampire {
-  constructor(name) {
+  constructor(name, pet="bat") {
     this.name = name;
+    this.pet = pet;
+    this.thirsty = true;
+    this.ouncesDrank = 0;
+  }
+
+  drink() {
+    if (this.ouncesDrank === 50) {
+      return "I\'m too full to drink anymore!"
+    } else {
+      this.thirsty = false;
+      this.ouncesDrank += 10;
+    }
   }
 }
 
