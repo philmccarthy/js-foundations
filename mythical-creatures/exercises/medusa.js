93d304e87311097cdba42d59755cd4af13915595
Phil McCarthy <philmcc2631@gmail.com>
2021-05-28 11:13:51 -0600

    Add medusa solutions

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,19 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victim) {
+    this.statues.push(new Statue(victim.name));
+    if (this.statues.length > 3 ) {
+      var freedPerson = this.statues.shift()
+      return new Person(freedPerson.name, 'relieved')
+    }
+  }
+}
+
+module.exports = Medusa;
